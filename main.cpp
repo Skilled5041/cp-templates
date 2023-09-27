156ff1fb4090cf18854fb1e6dbf6edf13fe5ac27
Skilled5041 <93156908+Skilled5041@users.noreply.github.com>
2023-09-27 09:10:32 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long i64;
+typedef unsigned long long u64;
+typedef double f64;
+typedef int i32;
+typedef float f32;
+typedef unsigned int u32;
+
+#define nl "\n"
+#define MOD 1000000007
+#define arr(T, n, i) T n[i] = {}
+#define vec(T, n) vector<T> n = {}
+#define p(a, b) pair<a, b>
+#define pi(x) printf("%d", x)
+#define pl(x) printf("%lld", x)
+#define pd(x) printf("%lf", x)
+#define ps(x) printf("%s", x)
+#define pc(x) printf("%c", x)
+#define pn printf("\n")
+
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+
+int main() {
+    return 0;
+}
